9221c537a77218ef5804c35b1a9d0ab55e2d10d8
Tarik Rajper <tarikmrajper@gmail.com>
2020-05-05 19:43:39 -0700

    Added Names to Main Class in Comment

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+/**
+ * Collaborators:
+ * Tarik Rajper
+ * Jerry Trieu
+ * Hyun-Kyung Oh
+ * Lawrence Chua
+ * Daniel Lecona
+ * Nghi Tran
+ * Sabrina Chan
+ */
 public class Main {
 
     public static void main(String[] args) {
         new UserInterface().startPSS();
     }
-
 }
